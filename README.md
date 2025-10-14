56b1ad7119bc7243dc51b620387e87ac114c7b66
kimi <kljeung@ucsc.edu>
2025-10-14 22:55:12 +0000

    added a header and canvas (step 1)

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CMPM 121 D2 Project
 
-This is the starting point for your CMPM 121 D2 project. Students should make sure to remove _this_ content from their README.md file and replace it with their own project documentation.
+## Step 1:
+
+- Added a header and ~~stole~~ 'borrowed' the canvas code from https://github.com/rndmcnlly/cmpm-121-f25-quaint-paint/blob/main/paint0.html
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
-
+//canvas code from https://github.com/rndmcnlly/cmpm-121-f25-quaint-paint/blob/main/paint0.html
 document.body.innerHTML = `
+  <h1> Sticker Sketching </h1>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.style.cursor = "none";
+document.body.append(canvas);
