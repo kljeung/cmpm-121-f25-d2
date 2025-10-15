39f1be73d62b15d2ded081931df970b04f059796
kimi <kljeung@ucsc.edu>
2025-10-15 06:05:58 +0000

    added drawing ability and clearing canvas (step 2)

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Step 1:
 
 - Added a header and ~~stole~~ 'borrowed' the canvas code from https://github.com/rndmcnlly/cmpm-121-f25-quaint-paint/blob/main/paint0.html
+
+## Step 2
+
+- added drawing and a clear canvas function
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,54 @@ import "./style.css";
 document.body.innerHTML = `
   <h1> Sticker Sketching </h1>
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <button id="button">Clear</button>
 `;
 
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
-canvas.style.cursor = "none";
+//canvas.style.cursor = "none";
 document.body.append(canvas);
+
+const context = canvas.getContext("2d");
+if (!context) {
+  throw new Error("I hate deno, let me commit >:(");
+}
+const button = document.getElementById("button");
+if (!button) {
+  throw new Error("deno, please for the love of everything, stop complaining");
+}
+let isDraw = false;
+let lastX = 0;
+let lastY = 0;
+
+type stupidTypeError = {
+  offsetX: number;
+  offsetY: number;
+};
+
+canvas.addEventListener("mousedown", (coord: unknown) => {
+  const cords = coord as stupidTypeError;
+  isDraw = true;
+  [lastX, lastY] = [cords.offsetX, cords.offsetY];
+});
+
+canvas.addEventListener("mouseout", () => (isDraw = false));
+canvas.addEventListener("mouseup", () => (isDraw = false));
+
+canvas.addEventListener("mousemove", (coord: unknown) => {
+  const cords = coord as stupidTypeError;
+  if (!isDraw) return;
+  context.beginPath();
+  context.moveTo(lastX, lastY);
+  context.lineTo(cords.offsetX, cords.offsetY);
+  context.strokeStyle = "black";
+  context.lineWidth = 2;
+  context.lineCap = "round";
+  context.stroke();
+  [lastX, lastY] = [cords.offsetX, cords.offsetY];
+});
+
+button.addEventListener("click", () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+});
